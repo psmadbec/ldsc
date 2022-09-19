3b3c60c7fd09c9a8cc470748da1524a1ffe9704c
psmadbec <psmadbec@broadinstitute.org>
2022-09-19 14:24:50 -0400

    Remove correlation scratch as it is in aggregator production

--- correlation_scratch/scripts/fetch_snp_file.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-aws s3 cp s3://dig-analysis-data/out/varianteffect/snp/ ../local_files/ --recursive --exclude="_SUCCESS"
-rm -f ../local_files/snp.csv
-head -n 1 ../local_files/part-*.csv | uniq >> ../local_files/snp.csv
-tail -n +2 ../local_files/part-*.csv >> ../local_files/snp.csv
-rm ../local_files/part-*.csv
